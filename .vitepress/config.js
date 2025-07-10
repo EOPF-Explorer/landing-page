694d6bef8baeb6be2c93b563ed8e2e31b183f854
silvester-pari <silvester.pari@eox.at>
2025-07-10 18:01:59 +0200

    fix(ci): base path

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vitepress";
 import baseConfig from "@eox/pages-theme-eox/config";
 
+// https://vitepress.dev/reference/site-config
 export default defineConfig({
   extends: baseConfig("eopf"),
   // Change the page config here
+  base: "/landing-page/",
   themeConfig: {
     nav: [
       { text: "Documentation", link: "#" },
