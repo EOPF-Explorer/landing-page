7dfbd213e254507ca2e27a41a6b1d0d832409b57
emmanuelmathot <emmanuelmathot@users.noreply.github.com>
2026-01-09 13:45:07 +0000

    Deploy preview for PR 31 🛫

--- pr-preview/pr-31/assets/chunks/proj4.Dg0Z8G9Q.js
+++
@@ -1,1 +0,0 @@
-import"./XYZ.DipmalTG.js";import"./Group.BuEEX7y8.js";import"./index.jYV8uuws.js";async function o(r){throw new Error("Proj4 must be registered first with register(proj4)")}async function n(r){return await o()}export{n as fromEPSGCode,o as fromProjectionCode};
