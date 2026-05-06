275003e8273823e0810e382a8e0dbaf6254c50d2
A-Behairi <behairiahmed@gmail.com>
2026-05-06 09:12:47 +0200

    chore: move to prod endpoint

--- sentinelexplorer/sentinel-explorer-config.js
+++ sentinelexplorer/sentinel-explorer-config.js
@@ -68,7 +68,7 @@ export default /*** @type {import("@eodash/eodash").Eodash} */ ({
   stacEndpoint: {
     endpoint: "https://api.explorer.eopf.copernicus.eu/stac",
     api: true,
-    rasterEndpoint: "https://api.explorer.eopf.copernicus.eu/rstaging",
+    rasterEndpoint: "https://api.explorer.eopf.copernicus.eu/raster",
     supportedUpscalingEndpoints: ["api.explorer.eopf.copernicus.eu/raster/"],
   },
   brand: {
